80a5a0be80b68a8880386abac6154783648be7bf
smichaelonline <stephaniemichael001@gmail.com>
2023-02-11 00:45:10 -0500

    Add Images to app. Update model to include coordinates.

--- Landmarks/Models/Landmark.swift
+++ Landmarks/Models/Landmark.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import SwiftUI
+import CoreLocation
 
 struct Landmark: Hashable, Codable {
   var id: Int
@@ -13,4 +15,21 @@ struct Landmark: Hashable, Codable {
   var park: String
   var state: String
   var description: String
+  
+  private var imageName: String
+  var image: Image {
+    Image(imageName)
+  }
+  
+  private var coordinates: Coordinates
+  var locationCoordinate: CLLocationCoordinate2D {
+    CLLocationCoordinate2D (
+      latitude: coordinates.latitude,
+      longitude: coordinates.longiture)
+  }
+  
+  struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longiture: Double
+  }
 }
