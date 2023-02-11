3d1d1c69cab08048345b6cde845b3fb22a188dcd
smichaelonline <stephaniemichael001@gmail.com>
2023-02-11 00:02:32 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Stephanie Michael on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
