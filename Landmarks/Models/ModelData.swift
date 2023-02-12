f8992c3d4284366c4acf39865e49586452ee126c
smichaelonline <stephaniemichael001@gmail.com>
2023-02-12 02:01:45 -0500

    Create and style category row view, update models to include category information

--- Landmarks/Models/ModelData.swift
+++ Landmarks/Models/ModelData.swift
@@ -12,6 +12,13 @@ final class ModelData: ObservableObject{
   //creates an array of landmarks that are initialized from the json file
   @Published var landmarks: [Landmark] = load("landmarkData.json")
   var hikes: [Hike] = load("hikeData.json")
+  
+  var categories: [String: [Landmark]] {
+    Dictionary(
+      grouping: landmarks,
+      by: { $0.category.rawValue }
+    )
+  }
 }
 
 // load method fetches JSON data from the app's main bundle
---
+++ Landmarks/Views/Categories/CategoryItem.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by Stephanie Michael on 2/12/23.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem()
+    }
+}
