d73415c0ba714f5d3ee84ef6f8659d82921748c1
smichaelonline <stephaniemichael001@gmail.com>
2023-02-11 23:08:24 -0500

    Create struct for badge shape and import CoreGraphics

---
+++ Landmarks/Views/HexagonParameters.swift
@@ -0,0 +1,17 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by Stephanie Michael on 2/11/23.
+//
+
+//import CoreGraphics to use CGPoint.
+import CoreGraphics
+
+struct HexagonParameters {
+  struct Segment {
+    let line: CGPoint
+    let curve: CGPoint
+    let control: CGPoint
+  }
+}
