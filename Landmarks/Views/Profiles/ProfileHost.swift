bd6f7d115ad33d9b7e7f6d2681b750ba00c7c6d8
smichaelonline <stephaniemichael001@gmail.com>
2023-02-12 03:22:59 -0500

    Complete edit functionality

--- Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -9,19 +9,31 @@ import SwiftUI
 
 struct ProfileHost: View {
   @Environment(\.editMode) var editMode
-  @EnvironmentObject var modelDate: ModelData
+  @EnvironmentObject var modelData: ModelData
   @State private var draftProfile = Profile.default
   
   var body: some View {
     VStack(alignment: .leading, spacing: 20){
       HStack {
+        if editMode?.wrappedValue == .active {
+          Button("Cancel", role: .cancel) {
+            draftProfile = modelData.profile
+            editMode?.animation().wrappedValue = .inactive
+          }
+        }
         Spacer()
         EditButton()
       }
       if editMode?.wrappedValue == .inactive {
-        ProfileSummary(profile: modelDate.profile)
+        ProfileSummary(profile: modelData.profile)
       } else {
         ProfileEditor(profile: $draftProfile)
+          .onAppear{
+            draftProfile = modelData.profile
+          }
+          .onDisappear{
+            modelData.profile = draftProfile
+          }
       }
     }
     .padding()
