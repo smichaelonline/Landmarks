5f1cd022888222df6e695f8787df806c7563d4a3
smichaelonline <stephaniemichael001@gmail.com>
2023-02-13 17:18:02 -0500

    Add additional notes

--- Landmarks/Views/Helpers/MapView.swift
+++ Landmarks/Views/Helpers/MapView.swift
@@ -9,19 +9,24 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+  // creates a location coordinate object referred to in function below
   var coordinate: CLLocationCoordinate2D
+  //variable to hold the region information for map view
   @State private var region = MKCoordinateRegion()
   
   var body: some View {
+    // view returns new coordinates before view appears to user
     Map(coordinateRegion: $region)
       .onAppear{
         setRegion(coordinate)
       }
   }
   
+  // function sets region as pinpointed by coordinates in data file.
   private func setRegion(_ coordinate: CLLocationCoordinate2D) {
     region = MKCoordinateRegion(
       center: coordinate,
+      // indicates zoom level
       span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
     )
   }
@@ -29,6 +34,6 @@ struct MapView: View {
 
 struct MapView_Previews: PreviewProvider {
   static var previews: some View {
-   MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
+    MapView(coordinate: CLLocationCoordinate2D(latitude: 59.980167, longitude: -152.665167))
   }
 }
